25de1e30227df63821f8708ae9788c9f657813b9
BGV <26331505+ed789d0@users.noreply.github.com>
2024-02-05 21:29:35 -0500

    added title mod

--- content_script.js
+++ content_script.js
@@ -1,8 +1,12 @@
 // Put all the javascript code here, that you want to execute after page load.
+if (window.location.pathname.match(new RegExp("/courses/.+/assignments/.+/submissions/.+"))) {
+  let assignmentName = document.querySelector(".submissionOutlineHeader--assignmentTitle").innerText
+  document.title = assignmentName + " | " + document.title
+}
 document.querySelectorAll(".courseBox--assignments").forEach(async (box) => {
-    let coursehtml = await fetch(box.parentElement.href)
-      coursehtml = await coursehtml.text()
-      let courseDOM = new DOMParser().parseFromString(coursehtml, 'text/html')
-      let todoCount = courseDOM.querySelectorAll(".submissionStatus-warning + td .progressBar").length
-      box.innerHTML += " (" + todoCount + " to do)"
-  })
+  let coursehtml = await fetch(box.parentElement.href)
+  coursehtml = await coursehtml.text()
+  let courseDOM = new DOMParser().parseFromString(coursehtml, 'text/html')
+  let todoCount = courseDOM.querySelectorAll(".submissionStatus-warning + td .progressBar").length
+  box.innerHTML += " (" + todoCount + " to do)"
+})
